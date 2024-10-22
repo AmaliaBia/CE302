029ad28ff0756eae6789398c8d3a0cecabfce3ec
AmaliaBia <amalia.beatriz@ufpr.br>
2024-10-21 22:14:27 -0300

    .

--- Notas de aula/Aula Ara.R
+++ Notas de aula/Aula Ara.R
@@ -67,4 +67,74 @@ links=tibble(source=c(0,1,2,3,4,4,6,6,7,7,7,8,8,8), target=c(4,4,4,4,5,6,7,8,9,1
 S=sankeyNetwork(Links = links, Nodes=nos, Source = "source", Target = "target", Value = "value", NodeID = "names", units = "Bi", fontSize = 12, nodeWidth = 10)
 S
 
+library(networkD3)
+library(dplyr)
+
+# Criar os nós
+nos <- tibble(nomes = c("EUA e Canadá",   # 0
+                        "Europa, OM, e África",  # 1
+                        "LATAM",  # 2
+                        "Ásia e Pacífico",  # 3
+                        "Receita",  # 4
+                        "Lucro Bruto",  # 5
+                        "CMV",  # 6
+                        "Lucro Operacional",  # 7
+                        "Custo Operacional",  # 8
+                        "Lucro Líquido",  # 9
+                        "IR",  # 10
+                        "Juros",  # 11
+                        "Marketing",  # 12
+                        "Tech e Dev",  # 13
+                        "Adm e Geral"))  # 14
+
+# Criar os links entre os nós
+links <- tibble(source = c(0, 1, 2, 3,  # Regiões
+                           4, 4,        # Receita
+                           5, 5,        # Lucro Bruto
+                           7, 7, 7,     # Lucro Operacional
+                           8, 8, 8),    # Custo Operacional
+                
+                target = c(4, 4, 4, 4,   # Convergência Receita
+                           5, 6,         # Convergência Lucro Bruto e CMV
+                           7, 8,         # Lucro Operacional e Custo Operacional
+                           9, 10, 11,    # Lucro Líquido, IR e Juros
+                           12, 13, 14),  # Marketing, Tech e Adm
+                
+                valores = c(4.3, 3.0, 1.2, 1.1,  # Valores Receita
+                            4.4, 5.2,           # Valores Lucro Bruto e CMV
+                            2.6, 1.8,           # Lucro Operacional e Custo
+                            2.1, 0.4, 0.089,    # Lucro Líquido, IR e Juros
+                            0.6, 0.7, 0.4))     # Marketing, Tech e Adm
+
+# Criar o gráfico de Sankey
+sankey <- sankeyNetwork(Links = links, Nodes = nos, 
+                        Source = "source", Target = "target", 
+                        Value = "valores", NodeID = "nomes", 
+                        units = "Bi", fontSize = 12, nodeWidth = 30)
+
+# Exibir o gráfico
+sankey
+
 #################################
+
+
+require(ggplot2)
+require(dplyr)
+
+data(mtcars)
+head(mtcars)
+#ctrl+shift+= %>% 
+#dispersao
+mtcars %>% ggplot(aes(x=hp, y=mpg))+geom_point()
+#ou um pouco melhor
+mtcars %>% ggplot(aes(x=hp, y=mpg,size = am,colour = factor(am)))+ #base crua do plot
+  geom_point()+ #define o grafico de dispersao
+  xlab("potencia")+ylab("milhas por galao")+ggtitle("grafico de dispersao")+ #nomes
+  scale_y_log10()+scale_x_continuous(limits = c(50,400))+ #tamano escala x e y
+  scale_size_continuous("quarto por milha")+ #modifica o size do comeco
+  scale_color_manual("cambio",labels=c("man","aut"), values = c("blue","lightgreen"))+ #modifica o color do comeco+identificacao
+  facet_grid(facets = ~am)+ #divide em duas grades
+  theme_dark()+ #background
+  theme(text = element_text(size=15, color="blue"), #alteracao do texto geral
+        plot.title = element_text(hjust = 0.5,size = 15, color = "purple")) #alteracao do titulo
+
